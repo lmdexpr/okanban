06e111779e854bb295d64953d7c82e96089691c4
lmdexpr <lmdexpr@gmail.com>
2025-05-09 18:24:05 +0900

    enable nix develop

--- flake.nix
+++ flake.nix
@@ -1,95 +1,117 @@
 {
-  description = "OKanban - Simple Kanban app written in OCaml";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    opam-repository = { url = "github:ocaml/opam-repository"; flake = false; };
+
     flake-utils.url = "github:numtide/flake-utils";
+
+    opam-nix = {
+      url = "github:tweag/opam-nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+        opam-repository.follows = "opam-repository";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, flake-utils, opam-nix, nixpkgs, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        ocamlPackages = pkgs.ocaml-ng.ocamlPackages_5_3;
+        on = opam-nix.lib.${system};
         
-        # Frontend dependencies
-        nodeDependencies = (pkgs.callPackage ./client/default.nix {}).nodeDependencies;
+        src = ./.;
+        localName = "okanban";
+
+        localPackagesQuery = {
+          ${localName} = "*";
+        };
+
+        devPackagesQuery = {
+          ocaml-base-compiler = "*";
+          ocaml-lsp-server = "*";
+        };
+
+        query = devPackagesQuery // localPackagesQuery;
+
+        overlay = self: super:
+          with builtins;
+          let
+            super' = mapAttrs
+              (p: _:
+                if hasAttr "passthru" super.${p} && hasAttr "pkgdef" super.${p}.passthru
+                then super.${p}.overrideAttrs (_: { opam__with_test = "false"; opam__with_doc = "false"; })
+                else super.${p})
+              super;
+            local' = mapAttrs
+              (p: _:
+                super.${p}.overrideAttrs (_: {
+                  doNixSupport = false;
+                }))
+              localPackagesQuery;
+          in
+          super' // local';
+
+        scope =
+          let
+            scp = on.buildOpamProject'
+              {
+                inherit pkgs;
+                resolveArgs = { with-test = true; with-doc = true; };
+                pinDepends = true;
+              }
+              src
+              query;
+          in
+          scp.overrideScope overlay;
+
+        devPackages = builtins.attrValues
+          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope);
+
+        okanban-client = pkgs.buildNpmPackage {
+          pname = "okanban-client";
+          version = "0.1.0";
+          src = ./client;
+
+          npmDepsHash = "sha256-oVFQDov55ROy5A9POBdBat3HkGSnhWNCb9/O8N7nmE8=";
+
+          buildPhase = ''
+            runHook preBuild
+            npm run build
+            npm run webpack
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/public
+            cp ../public/bundle.js $out/public/bundle.js
+            cp -r ../public/index.html $out/ || true
+            cp -r ../public/styles.css $out/ || true
+            runHook postInstall
+          '';
+        };
       in {
+        legacyPackages = pkgs;
+
         packages = {
           default = self.packages.${system}.okanban;
           
-          okanban = pkgs.stdenv.mkDerivation {
-            name = "okanban";
-            src = ./.;
-            
-            nativeBuildInputs = with pkgs; [
-              ocamlPackages.dune_3
-              ocamlPackages.findlib
-              ocamlPackages.ocaml
-              nodejs
-              nodePackages.npm
+          okanban = scope.${localName};
+
+          okanban-client = okanban-client;
+        };
+        
+        devShells.default =
+          pkgs.mkShell {
+            inputsFrom = builtins.map (p: scope.${p}) [ localName ];
+            buildInputs = devPackages ++ [
+              pkgs.nodePackages.npm
+              pkgs.nil pkgs.nixpkgs-fmt
+              pkgs.rescript-language-server
             ];
-            
-            buildInputs = with ocamlPackages; [
-              dream
-              yojson
-              ppx_deriving_yojson
-              ppx_fields_conv
-              odoc
-            ];
-            
-            buildPhase = ''
-              # Build OCaml backend
-              dune build
-              
-              # Build frontend
-              mkdir -p client/node_modules
-              cp -r ${nodeDependencies}/* client/node_modules/
-              chmod -R +w client/node_modules
-              
-              cd client
-              npm run build
-              npm run webpack
-              cd ..
-            '';
-            
-            installPhase = ''
-              mkdir -p $out/bin
-              mkdir -p $out/public
-              
-              cp -r _build/default/bin/main.exe $out/bin/okanban
-              cp -r public/* $out/public/
-            '';
           };
-        };
-        
-        apps.default = {
-          type = "app";
-          program = "${self.packages.${system}.okanban}/bin/okanban";
-        };
-        
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            # OCaml
-            ocamlPackages.ocaml
-            ocamlPackages.dune_3
-            ocamlPackages.findlib
-            ocamlPackages.dream
-            ocamlPackages.yojson
-            ocamlPackages.ppx_deriving_yojson
-            ocamlPackages.ppx_fields_conv
-            ocamlPackages.odoc
-            ocamlPackages.ocaml-lsp
-            ocamlPackages.ocamlformat
-            
-            # Node.js
-            nodejs
-            nodePackages.npm
-            nodePackages.typescript
-            nodePackages.webpack
-            nodePackages.webpack-cli
-          ];
-        };
       }
     );
 }
